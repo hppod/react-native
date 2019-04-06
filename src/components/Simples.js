50e7cd7854b245436ce813c5969f928652298541
hppod <hppodgornik@gmail.com>
2019-04-06 15:13:27 -0300

    Utilizando arrow function em componente funcional

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text } from 'react-native'
 
 //DOIS TIPOS DE COMPONENTE
 //BASEADO EM CLASSE COMO O APP.JS
@@ -7,6 +7,13 @@ import { Text } from 'react-native'
 
 //PROPS É PARA RECEBER PROPRIEDADES NO COMPONENTE. ACESSA AS PROPRIEDADES QUE SÃO PASSADAS. INTERPOLAÇÃO DENTRO DA TAG TEXT
 
-export default function (props) {
-    return <Text>{props.texto}</Text>
-}
+// export default function (props) {
+//     return <Text>{props.texto}</Text>
+// }
+
+//UTILIZANDO ARROW FUNCTION
+export default props =>
+    <View>
+        <Text>Arrow 1: {props.texto}</Text>
+        <Text>Arrow 2: {props.texto}</Text>
+    </View>
