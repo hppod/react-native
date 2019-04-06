1bb1e06d9260aa7f6f41641bf4058d3722ae9b47
hppod <hppodgornik@gmail.com>
2019-04-06 15:01:57 -0300

    implementando o simples.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+//DOIS TIPOS DE COMPONENTE
+//BASEADO EM CLASSE COMO O APP.JS
+//BASEADO EM FUNÇÃO, COMO SIMPLES.JS
+
 export default class App extends Component {
   render() {
     return (
---
+++ src/components/Simples.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+//DOIS TIPOS DE COMPONENTE
+//BASEADO EM CLASSE COMO O APP.JS
+//BASEADO EM FUNÇÃO, COMO SIMPLES.JS
+
+export default function () {
+    return <Text>Simples!</Text>
+}
