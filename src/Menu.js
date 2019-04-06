5669d0983591a3b85d9f2f4fd725e37c557d1d4d
hppod <hppodgornik@gmail.com>
2019-04-06 20:10:08 -0300

    Comunicação direta entre componentes pais e filhos

--- src/Menu.js
+++ src/Menu.js
@@ -8,8 +8,13 @@ import Contador from './components/Contador'
 import Plataformas from './components/Plataformas'
 import ValidarProps from './components/ValidarProps'
 import Evento from './components/Evento'
+import { Avo } from './components/ComunicacaoDireta'
 
 export default createDrawerNavigator({
+    Avo: {
+        screen: () => <Avo nome='João' sobrenome='Silva' />,
+        navigationOptions: { title: 'Familia' }
+    },
     Evento: {
         screen: () => <Evento />
     },
