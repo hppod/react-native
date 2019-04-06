f51e0a7daf0fe25139a7e501f2b36c589cf0ad24
hppod <hppodgornik@gmail.com>
2019-04-06 15:10:28 -0300

    PROPS

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -5,6 +5,8 @@ import { Text } from 'react-native'
 //BASEADO EM CLASSE COMO O APP.JS
 //BASEADO EM FUNÇÃO, COMO SIMPLES.JS
 
+//PROPS É PARA RECEBER PROPRIEDADES NO COMPONENTE. ACESSA AS PROPRIEDADES QUE SÃO PASSADAS. INTERPOLAÇÃO DENTRO DA TAG TEXT
+
 export default function (props) {
     return <Text>{props.texto}</Text>
 }
