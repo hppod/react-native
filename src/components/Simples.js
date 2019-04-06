f6880e501a30a3c994d0568f79110ba87e72bf4e
hppod <hppodgornik@gmail.com>
2019-04-06 15:37:26 -0300

    criando stylesheet para componentes e aplicando ele ao componente simples

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../estilo/Padrao'
 
 //DOIS TIPOS DE COMPONENTE
 //BASEADO EM CLASSE COMO O APP.JS
@@ -12,7 +13,4 @@ import { Text } from 'react-native'
 // }
 
 //UTILIZANDO ARROW FUNCTION
-export default props => [
-    <Text key={1}>Arrow 1: {props.texto}</Text>,
-    <Text key={2}>Arrow 2: {props.texto}</Text>
-]
+export default props => <Text style={[Padrao.ex]}>Arrow 1: {props.texto}</Text>
